31b457645f4bc817954beae321b9f005c3736f3c
sanicboi <alexandrzezekalo@gmail.com>
2023-06-30 12:41:58 +0000

    Added User controller

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -12,7 +12,7 @@ export class UserController {
     }
 
 
-    public async one(chatId: string): Promise<User|boolean> {
+    public async one(chatId: string): Promise<User|false> {
         try {
             const user = await this.repo.findOne({
                 where: {
@@ -26,5 +26,83 @@ export class UserController {
         }
     }
 
-    
+    public async create(chatId: string, name: string, tgName: string): Promise<boolean> {
+        const user = new User();
+        user.chatId = chatId;
+        user.name = name;
+        user.enteredPromo = false;
+        user.enteredStats = false;
+        user.enteringStats = false;
+        user.enteredToken = false;
+        user.enteringToken = false;
+        user.registeredWithoutPromo = false;
+        user.signupDate = new Date();
+        user.name = name;
+        user.special = false;
+        user.tgName = tgName;
+        try {
+            await this.repo.save(user);
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    public async startEnteringToken(chatId: string, type: "basic" | "stats"): Promise<boolean> {
+        const user = await this.one(chatId);
+        if (!user) return false;
+        
+        if (type === "basic") {
+            user.enteringToken = true;
+        } else {
+            user.enteredStats = true;
+        }
+
+        try {
+            await this.repo.save(user);
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    public async stopEnteringTokens(chatId: string): Promise<boolean> {
+        const user = await this.one(chatId);
+        if (!user) return false;
+
+        user.enteringToken = false;
+        user.enteredStats = false;
+
+        try {
+            await this.repo.save(user);
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    public async enterPromo(chatId: string, type: "basic" | "special" | "nopromo"): Promise<boolean> {
+        const user = await this.one(chatId);
+        if (!user) return false;
+
+        if (type === "basic") {
+            user.enteredPromo = true;
+        } else if (type === "special") {
+            user.enteredPromo = true;
+            user.special = true;
+        } else {
+            user.registeredWithoutPromo = true;
+        }
+
+        try {
+            await this.repo.save(user);
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
 }
